1891e631033fcbbdbbdf25721d19376a4e6ca670
Arturo Jamaica <me@arturojamaica.com>
2022-02-22 17:50:32 +0900

    Introduce Coldwallet signature provider

--- packages/solana/lib/src/signer/signer_cold_wallet.dart
+++ packages/solana/lib/src/signer/signer_cold_wallet.dart
@@ -1,14 +1,29 @@
+// ignore_for_file: sort_constructors_first
+
+import 'dart:async';
+
 import 'package:solana/src/encoder/message.dart';
 import 'package:solana/src/encoder/signature.dart';
 import 'package:solana/src/encoder/signed_tx.dart';
 import 'package:solana/src/signer/signer_base.dart';
 
+typedef ColdAsyncMessageSign = Future<SignedTx> Function({required Message message, required String recentBlockhash});
+typedef ColdAsyncSign = Future<Signature> Function({Iterable<int> data});
+
+abstract class ColdWalletSignatureProvider {
+  ColdAsyncMessageSign signMessage;
+  ColdAsyncSign sign;
+  ColdWalletSignatureProvider({required this.signMessage, required this.sign});
+}
+
 class SignerColdWallet extends Signer {
   SignerColdWallet({
+    required this.coldWalletSignatureDelegate,
     required String publicKey,
   }) : _publicKey = publicKey;
 
   final String _publicKey;
+  final ColdWalletSignatureProvider coldWalletSignatureDelegate;
 
   @override
   String get address => _publicKey;
@@ -20,12 +35,9 @@ class SignerColdWallet extends Signer {
   Future<SignedTx> signMessage({
     required Message message,
     required String recentBlockhash,
-  }) {
-    throw UnimplementedError();
-  }
+  }) =>
+      coldWalletSignatureDelegate.signMessage.call(message: message, recentBlockhash: recentBlockhash);
 
   @override
-  Future<Signature> sign(Iterable<int> data) {
-    throw UnimplementedError();
-  }
+  Future<Signature> sign(Iterable<int> data) => coldWalletSignatureDelegate.sign.call(data: data);
 }
